74226d9caf2fb5aa3384adcbfb622915074acdc8
Nikita <poituk15@gmail.com>
2025-07-29 17:14:21 +0300

    deploy

--- src/components/game/gameModule/specialBot.js
+++ src/components/game/gameModule/specialBot.js
@@ -1,16 +1,20 @@
 let ans;
+let dp = new Map();
+
 function dfs(field, startTime){
-    if (Date.now() - startTime >= 1000) return;
+    if (Date.now() - startTime >= 1000) return 0;
+    if (dp.has(field)) return dp[field];
     for (let i = 0; i < field.length; i++) {
         if (field[i] != 0) continue;
         let nArray = [...field];
         nArray[i] = 2;
         if (!dfs(nArray, startTime)) {
             ans = nArray;
+            dp[field] = 1;
             return 1;
         }
     }
-    if (Date.now() - startTime >= 1000) return;
+    if (Date.now() - startTime >= 1000) return 0;
     for (let i = 0; i < field.length; i++) {
         for (let j = i + 1; j < field.length; j++){
             if (field[i] != 0 || field[j] != 0) continue;
@@ -18,26 +22,30 @@ function dfs(field, startTime){
             nArray[i] = nArray[j] = 2;
             if (!dfs(nArray, startTime)) {
                 ans = nArray;
+                dp[field] = 1;
                 return 1;
             }
         }
     }
-    if (Date.now() - startTime >= 1000) return;
+    if (Date.now() - startTime >= 1000) return 0;
     for (let i = 0; i < field.length - 2; i++) {
         if (field[i] != 0) continue;
         let nArray = [...field];
         nArray[i] = nArray[i + 1] = nArray[i + 2] = 2;
         if (!dfs(nArray, startTime)) {
             ans = nArray;
+            dp[field] = 1;
             return 1;
         }
     }
+    dp[field] = 0;
     return 0;
 }
 
 export default function specialBot(field) {
     if(!dfs(field, Date.now())) {
         ans = [...field];
+        conosole.log('lose');
         for (let i = 0; i < ans.length; i++) {
             if (ans[i] != 0) continue;
             ans[i] = 2;
